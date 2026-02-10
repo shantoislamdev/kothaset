21f02f578ae53004c52b1a548af3350dd226dd2f
Shanto Islam <shantoislamdev@gmail.com>
2026-02-10 20:34:03 +0600

    chore(cli): remove unused contains functions
    Remove unused contains() and containsHelper() functions from init.go

--- internal/cli/init.go
+++ internal/cli/init.go
@@ -207,16 +207,3 @@ func gitignoreContains(content, entry string) bool {
 	}
 	return false
 }
-
-func contains(s, substr string) bool {
-	return len(s) >= len(substr) && (s == substr || len(s) > 0 && containsHelper(s, substr))
-}
-
-func containsHelper(s, substr string) bool {
-	for i := 0; i <= len(s)-len(substr); i++ {
-		if s[i:i+len(substr)] == substr {
-			return true
-		}
-	}
-	return false
-}
